a385a6098b88ac17c66cd0644867a14c07de8d12
Kevin Bentlage <kevin@bentlage.nu>
2017-10-12 16:59:46 +0200

    Refactored install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create log directory
-mkdir /usr/local/directadmin/plugins/ssh_key_management/logs/
+mkdir /usr/local/directadmin/plugins/ssh_key_management/logs
 
 # Fix ownerships
-chown -R diradmin.diradmin /usr/local/directadmin/plugins/ssh_key_management/*
+chown -R diradmin.diradmin /usr/local/directadmin/plugins/ssh_key_management
 
 # Fix permissions
 chmod -R 0775 /usr/local/directadmin/plugins/ssh_key_management/user/*
