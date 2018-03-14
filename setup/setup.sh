ba0cdf4b6a25d9397a944b37baac172c4efef9e7
Kevin Bentlage <kevin@bentlage.nu>
2018-03-14 23:56:25 +0100

    Bugfix: copy-ing over crontab file won't work, echoing into file now.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Enable crontab
-cp /usr/local/directadmin/plugins/ssh_key_management/setup/da-ssh-key-management_cron /etc/cron.d/
+echo "* * * * * root /usr/local/directadmin/plugins/ssh_key_management/php/Cronjobs/cron.php" > /etc/cron.d/da-ssh-key-management_cron
